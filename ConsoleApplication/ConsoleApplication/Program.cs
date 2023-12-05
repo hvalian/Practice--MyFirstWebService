747ad28a923c6b712808ae52b5625d833e0e30ee
Homayoon Valian <hvalian@perfectsystem.scom>
2023-12-04 22:05:49 -0500

    Initial files

---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebServiceTest.TestWeb;
+
+namespace WebServiceTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Service1 webService = new Service1();
+            string srt = webService.SimpleMethod("homayoon");
+            Console.WriteLine(srt);
+            Console.WriteLine(webService.AnotherSimpleMethod (1,2));
+
+        }
+    }
+}
---
+++ MyFirstWebService/MyFirstWebService/Service1.asmx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace MyFirstWebService
+{
+    /// <summary>
+    /// Summary description for Service1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Service1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public string SimpleMethod(string srt)
+        {
+            return "Hello " + srt;
+        }
+
+        [WebMethod]
+        public int AnotherSimpleMethod(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
